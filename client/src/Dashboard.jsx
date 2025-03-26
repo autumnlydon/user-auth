d628aee839649847f1dc2471478785a9515bf883
Autumn Lydon <autumnlydon@gmail.com>
2025-03-26 17:32:45 -0400

    dashboard functionality works

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,7 +1,43 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Dashboard() {
-    const navigate = useNavigate();
-    
+    const [dashboardData, setDashboardData] = useState(null);
+
+    useEffect(() => {
+        const fetchDashboardData = async () => {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                return setDashboardData("You are not logged in");
+            }
+            try {
+                const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}api/auth/dashboard`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                const data = await response.json();
+            if (response.ok) {
+                setDashboardData(data);
+            } else {
+                alert('Failed to fetch dashboard data');
+            }
+        } catch (error) {
+            console.error('Error fetching dashboard data:', error);
+                alert('Failed to fetch dashboard data');
+            }
+        }
+        fetchDashboardData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {dashboardData ? (
+                <p>{dashboardData.message}</p>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    )
 }
